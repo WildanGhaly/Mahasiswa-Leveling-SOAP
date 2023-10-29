2f601781e123d5327d4ec6fbb5734c5a05f4417f
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-29 18:57:36 +0700

    feat: add dummy ChallengeDAO and ChallengeDAOImpl

---
+++ src/main/java/org/example/model/ChallengeDAO.java
@@ -0,0 +1,5 @@
+package org.example.model;
+
+public interface ChallengeDAO extends GenericDAO<Challenge> {
+    
+}
---
+++ src/main/java/org/example/model/ChallengeDAOImpl.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+import java.util.List;
+
+public class ChallengeDAOImpl implements ChallengeDAO {
+
+    @Override
+    public void insert(Challenge entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public Challenge findById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public List<Challenge> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public void update(Challenge entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+    
+}
