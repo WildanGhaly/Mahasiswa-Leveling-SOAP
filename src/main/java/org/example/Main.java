e10993107e4d20d03cdc119eab40fc6b6b69f724
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-29 18:21:49 +0700

    feat: add test service main and update workspace

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,12 @@ package org.example;
 
 import javax.xml.ws.Endpoint;
 
+import org.example.service.ChallengeServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://localhost:8080/ws/testing", new TestingService());
+            Endpoint.publish("http://localhost:8081/ws/testing", new ChallengeServiceImpl());
             System.out.println("Server started");
         } catch (Exception e) {
             System.out.println("Something Wrong");
