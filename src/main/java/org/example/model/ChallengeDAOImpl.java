8696676c91a603a87991754172a5f79258a39859
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2023-10-31 09:28:07 +0700

    feat: add dummy data for soap test (find by id)

--- src/main/java/org/example/model/ChallengeDAOImpl.java
+++ src/main/java/org/example/model/ChallengeDAOImpl.java
@@ -1,8 +1,20 @@
 package org.example.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 
+import org.example.core.Database;
+
 public class ChallengeDAOImpl implements ChallengeDAO {
+
+    private Connection connection;
+
+    public ChallengeDAOImpl() {
+        Database db = new Database();
+        this.connection = db.getConnection();
+    }
 
     @Override
     public void insert(Challenge entity) {
@@ -12,8 +24,24 @@ public class ChallengeDAOImpl implements ChallengeDAO {
 
     @Override
     public Challenge findById(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+        Challenge challenge = null;
+        String sql = "SELECT * FROM challenge WHERE id = ?";
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    challenge = new Challenge (
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("description"),
+                        resultSet.getString("threshold")
+                    );
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return challenge;
     }
 
     @Override
--- src/main/java/org/example/service/ChallengeServiceImpl.java
+++ src/main/java/org/example/service/ChallengeServiceImpl.java
@@ -5,12 +5,15 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 
 import org.example.model.Challenge;
+import org.example.model.ChallengeDAOImpl;
+import org.example.core.Database;
 
 @WebService
 public class ChallengeServiceImpl implements ChallengeService {
     @Override
     @WebMethod
     public Challenge getChallengeById(int id) {
-        return new Challenge(id, "Challenge " + id, "Description " + id, "Threshold " + id);
+        ChallengeDAOImpl challengeDAO = new ChallengeDAOImpl();
+        return challengeDAO.findById(id);
     }
 }
