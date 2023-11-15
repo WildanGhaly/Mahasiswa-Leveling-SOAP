81fd73bba683972ce58d041a5a7658089b18b8bf
Haidar <16521522@mahasiswa.itb.ac.id>
2023-11-16 06:02:27 +0700

    feat: add topup and code endpoint publish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,14 +3,18 @@ package org.example;
 import javax.xml.ws.Endpoint;
 
 import org.example.service.ChallengeServiceImpl;
+import org.example.service.TopupServiceImpl;
+import org.example.service.restCodeServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Endpoint.publish("http://0.0.0.0:8081/ws/testing", new ChallengeServiceImpl());
+            Endpoint.publish("http://0.0.0.0:8081/topup", new TopupServiceImpl());
+            Endpoint.publish("http://0.0.0.0:8081/code", new restCodeServiceImpl());
+            
             System.out.println("Server started");
         } catch (Exception e) {
-            System.out.println("Something Wrong");
+            System.out.println("Something Wrong, error is " + e);
         }
     }
 }
